598562e90708f16f8ee875d52f182bff2e0a4a1d
Xhendor <rosendorsc@gmail.com>
2015-08-18 15:13:00 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<head>
+
+    <!--  asdfasdf  -->
+
+  <link rel="stylesheet" href="estilacho.css" type="text/css"/>
+</head>
+
+
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xhendor
+ * Date: 8/18/15
+ * Time: 14:31
+ */
+
+echo "<h1 id='A_' >#1-Bienvenidos a la Materia de Programación Web</h1>";
+echo "<h1 id='B_' >#2-Bienvenidos a la Materia de Programación Web</h1>";
+echo "<h1 id='C_'>#3-Bienvenidos a la Materia de Programación Web</h1>";
+echo "<h1 id='D_'>#4-Bienvenidos a la Materia de Programación Web</h1>";
+
+
+//CORRECTO
+for($x=0;$x<10;$x++){
+
+    echo "NUMERO:".$x;
+    echo "<br/>";
+
+}
+?>
+
+
+<div>
+
+    <h1>SOY EL HEADER DEL DIV1</h1>
+        <div>
+    <h1>SOY EL HEADER DEL DIV2</h1>
+<div>
+    <h1>SOY EL HEADER DEL DIV3</h1>
+</div>
+    <h1>SOY EL HEADER DEL DIV4</h1>
+        <div>
+
+</div>
+
+</body>
